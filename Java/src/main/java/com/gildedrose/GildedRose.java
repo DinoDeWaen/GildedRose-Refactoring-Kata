1ce12ea33c3f31c74e7134ccc4b6ccaba4baf3b9
dino <dino.dewaen@unifiedpost.com>
2021-03-14 21:41:21 +0100

    add double quality decrease to normal item method

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -15,9 +15,13 @@ class GildedRose {
             if (!isLegendaryItem(item)) {
                 item.sellIn = item.sellIn - 1;
             }
+
             if (isNormalItem(item)) {
                 if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                    if (item.sellIn < 0 && item.quality > 0) {
                         item.quality = item.quality - 1;
+                    }
                 }
             } else if (isAged_brie(item)) {
                 if (item.quality < 50) {
@@ -44,14 +48,11 @@ class GildedRose {
             }
 
 
+
             if (item.sellIn < 0) {
                 if (!isAged_brie(item)) {
                     if (!isConcertItem(item)) {
-                        if (item.quality > 0) {
-                            if (!isLegendaryItem(item)) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
+
                     } else {
                         item.quality = item.quality - item.quality;
                     }
