141178aad060375286fe04f0af8a8a2afa386478
dino <dino.dewaen@unifiedpost.com>
2021-03-14 22:11:15 +0100

    wrap legendary item

---
+++ Java/src/main/java/com/gildedrose/LegendaryItem.java
@@ -0,0 +1,13 @@
+package com.gildedrose;
+
+public class LegendaryItem implements GildedRoseItem{
+
+    @Override
+    public void updateQuality() {
+
+    }
+
+    public static GildedRoseItem of(Item item) {
+        return new LegendaryItem();
+    }
+}
